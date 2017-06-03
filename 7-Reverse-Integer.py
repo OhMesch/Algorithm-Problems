871b587a21bdad0a2a3ce51d5db0ddc657f889de
OhMesch <mze5111@gmail.com>
2017-06-03 12:41:09 -0400

    Updated solution to return 0 for a 32-bit overflow

--- 7-Reverse-Integer.py
+++ 7-Reverse-Integer.py
@@ -1,10 +1,10 @@
 #Problem Statement:
 # Reverse digits of an integer.
+# Return zero if the number overflows 32-bit
 
 class Solution(object):
 	def reverse(self, x):
 		
-		orignalInt = x
 		originalStr = str(x)
 		newStr = []
 		neg = 0
@@ -20,12 +20,18 @@ class Solution(object):
 			newStr.insert(0,'-')
 
 		newStr = ''.join(newStr)
+		if abs(int(newStr)) > 2147483647:
+			return(0)
 		return(int(newStr))
 
 driver = Solution()
 
-t1 = 123
+t1 = 1534236469
 t2 = -123
+t3 = -2147483648
+t4 = 1563847412
 
 print(driver.reverse(t1))
 print(driver.reverse(t2))
+print(driver.reverse(t3))
+print(driver.reverse(t4))
