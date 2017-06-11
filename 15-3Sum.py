27501bacb41de9306bf0aae8b72f51ede02dc3fb
OhMesch <mze5111@gmail.com>
2017-06-10 23:08:03 -0400

    Problems in progress

--- 15-3Sum.py
+++ 15-3Sum.py
@@ -1,20 +1,62 @@
+# # Problem Statement:
+# # Given an array of integers 
+# # Find all unique triplets in the array which gives the sum of zero.
+
+# class Solution:
+# 	def threeSum(self, nums):
+		
+# 		dictionary = {}
+# 		output = [[70]]
+# 		for i in range(len(nums)):
+# 			target = nums[i]
+# 			numsReduced = nums[0:i]
+# 			for k in range(i+1,len(nums)):
+# 				numsReduced.append(nums[k])
+# 			# 2 Sum
+# 			for j in range(len(numsReduced)):
+# 				if (target - nums[j]) in dictionary:
+# 					l = 0
+# 					while l < len(output):
+# 						print('werein')
+# 						if target not in output[l] or (target - nums[j]) not in output[l] or nums[j] not in output[l]:
+# 							output.append([target,target - nums[j],nums[j]])
+# 						l += 1
+# 				else:
+# 					dictionary[nums[j]] = j
+# 		return(output)
+
+
+# driver = Solution()
+# t1 = [-1,0,1,2,-1,-4]
+# # print(t1[])
+# print(driver.threeSum(t1))
+
 # Problem Statement:
 # Given an array of integers 
 # Find all unique triplets in the array which gives the sum of zero.
 
+# This part shows all combination of answers
+
 class Solution:
-	def threeSum(self, nums,target):
+	def threeSum(self, nums):
 		
 		dictionary = {}
 		output = []
-
-		for index in range(len(nums)):
-			if (target - nums[index]) in dictionary:
-				output.append([target - nums[index],nums[index]])
-			else:
-				dictionary[nums[index]] = index
+		for i in range(len(nums)):
+			target = nums[i]
+			numsReduced = nums[0:i]
+			for k in range(i+1,len(nums)):
+				numsReduced.append(nums[k])
+			# 2 Sum
+			for j in range(len(numsReduced)):
+				if (target - nums[j]) in dictionary:
+					output.append([target,target - nums[j],nums[j]])
+				else:
+					dictionary[nums[j]] = j
 		return(output)
+
 
 driver = Solution()
 t1 = [-1,0,1,2,-1,-4]
-print(driver.threeSum(t1,-2))
+# print(t1[])
+print(driver.threeSum(t1))
---
+++ Wk36-Q1.py
@@ -0,0 +1,49 @@
+# Merge two binary trees
+
+class TreeNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.left = None
+		self.right = None
+
+class Solution(object):
+	def mergeTrees(self, t1, t2):
+		arr1 = [None]*10000
+		arr2 = [None]*10000
+		self.recurse(t1,0,0,arr1)
+		self.recurse(t2,0,0,arr2)
+		output = arr1 + arr2
+		while output[-1] == None:
+			del output[-1]
+		while output[0] == None:
+			del output[0]
+
+
+	def recurse(self, t1, level,offset,arr):
+		# print(t1.val,level,offset)
+		print((2**(level+1)-1) + offset)
+		arr[2**(level+1)-1 + offset] = t1.val
+		if t1.left != None:
+			levl = level +1
+			offset = 2*offset
+			self.recurse(t1.left,levl,offset,arr)
+		if t1.right != None:
+			levr = level +1
+			offset = offset*2 +1
+			self.recurse(t1.right,levr,offset,arr)
+driver = Solution()
+
+tree = TreeNode(1)
+tree.right = TreeNode(2)
+tree.left = TreeNode(3)
+tree.left.left = TreeNode(5)
+
+tree2 = TreeNode(1)
+tree2.left = TreeNode(2)
+tree2.right = TreeNode(3)
+tree2.left.left = TreeNode(4)
+tree2.left.right = TreeNode(5)
+tree2.right.left = TreeNode(6)
+tree2.right.right = TreeNode(7)
+
+driver.mergeTrees(tree,tree2)
