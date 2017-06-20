4e52b0d733519c30621dbb3679cd208be124560e
OhMesch <mze5111@gmail.com>
2017-06-20 18:02:07 -0400

    Issue with min and max in same array

--- 624-Max-Distance-In-Arrays.py
+++ 624-Max-Distance-In-Arrays.py
@@ -2,83 +2,42 @@
 # Given an array of arrays, find the maximum difference between the numbers
 # Integers range from -10000 to 10000
 
-class Solution(object):
-    def maxDistance(self, arrays):
-        
-        minVal = 10001
-        maxVal = -10001
-        for arr in arrays:
-            print('current array is',arr)
-            if len(arr) == 0:
-                break
-            if min(arr) < minVal and max(arr) > maxVal:
-                print("min and max",minVal,maxVal)
-                diffMin = abs(min(arr)-minVal)
-                diffMax = abs(max(arr)-maxVal)
-                print('diffs are',diffMin,diffMax)
-                if diffMin <= diffMax:
-                    print('replace Max',max(arr))
-                    maxVal = max(arr) #Would setting a temp var to max and min be faster than calling function?
-                else:
-                    print('replace Min',min(arr))
-                    minVal = min(arr)
-            elif min(arr) < minVal:
-                minVal = min(arr)
-            elif max(arr) > maxVal:
-                maxVal = max(arr)
-            print('min and max values',minVal,maxVal)
+# Current issues: Will select values from the same array if it is one of first two non-empty arrays
 
+class Solution(object):
+	def maxDistance(self, arrays):
+		maxArr = []
+		minArr = []
+		for indvArr in arrays:
+			if len(indvArr) == 0:
+				continue
+			maxArr.append(max(indvArr))
+			minArr.append(min(indvArr))
+
+		print(arrays,'\nMax=',maxArr,'\nMin=',minArr) #---------------------------
+
+		absMax = max(maxArr)
+		absMin = min(minArr)
+
+		for index in range(len(minArr)):
+			
+			
 driver = Solution()
 t1 = [[1,2,3],[4,5],[1,2,3]]
 t2 = [[1,2,3],[],[1,2,3]]
 t3 = [[1,4],[0,5]]
 t4 = [[1,2,3],[8],[1,2,3]]
-print(driver.maxDistance(t1),'answer=4')
-print(driver.maxDistance(t2),'answer=2')
-print(driver.maxDistance(t3),'answer=4')
-print(driver.maxDistance(t4),'answer=7')
+t5 = [[7,0],[6,2]]
+t6 = [[7,7],[7,7]]
+t7 = [[7,0],[6,2],[7,0]]
 
-# class Solution(object):
-#     def maxDistance(self, arrays):
-#         dic = {}
-#         minVal = 10001
-#         maxVal = -10001
-#         for arrayIndex in range(len(arrays)):
-#         	if len(arrays[arrayIndex]) > 0:
-#         		dic[arrayIndex] = {}
-#         		dic[arrayIndex]['min'] = min(arrays[arrayIndex])
-#         		dic[arrayIndex]['max'] = max(arrays[arrayIndex])
-#         print(dic)
-#         for k,v in dic.items():
-#         	print('key',k,'values',v)
-#         	# if v['min'] < minVal:
-#         	# 	minVal = v['min']
-#         	# if v['max'] > maxVal:
-#         	# 	maxVal = v['max']
-#         	minVal = min(v['min'])
-#         	maxVal = max(v['max'])
-#         distance = abs(maxVal-minVal)
-#         return(distance)
+print('\n')
+print('\n',driver.maxDistance(t1),'answer=4\n')
+print('\n',driver.maxDistance(t2),'answer=2\n')
+print('\n',driver.maxDistance(t3),'answer=4\n')
+print('\n',driver.maxDistance(t4),'answer=7\n')
+print('\n',driver.maxDistance(t5),'answer=6\n')
+print('\n',driver.maxDistance(t6),'answer=0\n')
+print('\n',driver.maxDistance(t7),'answer=7\n')
 
-# class Solution(object):
-# 	def maxDistance(self, arrays):
-# 		min = 10001
-# 		max = -10001
-# 		for array in arrays:
-# 			if len(array) == 0:
-# 				break
-# 			for elm in array:
-# 				if elm < min:
-# 					min = elm
-# 				if elm > max:
-# 					max = elm 
-# 		range = abs(max-min)
-# 		return(range)
 
-# driver = Solution()
-# t1 = [[1,2,3],[4,5],[1,2,3]]
-# t2 = [[1,2,3],[],[1,2,3]]
-# t3 = [[1,4],[0,5]]
-# print(driver.maxDistance(t1))
-# print(driver.maxDistance(t2))
-# print(driver.maxDistance(t3))
