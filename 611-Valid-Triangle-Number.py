a40837af096ed58cd1e5a63ad1f26cb70077c2ae
OhMesch <mze5111@gmail.com>
2017-06-27 23:23:58 -0400

    Accepted solution

--- 611-Valid-Triangle-Number.py
+++ 611-Valid-Triangle-Number.py
@@ -13,23 +13,30 @@ class Solution(object):
 		occur = {}
 		unique = []
 		solCount = 0
+		solutions = []
 
 		nums.sort()
 
+		if len(nums) <3:
+			return(0)
+
 		for number in nums:
-			if number not in occur:
-				occur[number] = 1
-				unique.append(number)
-			else:
-				occur[number] += 1
+			if number != 0:
+				if number not in occur:
+					occur[number] = 1
+					unique.append(number)
+				else:
+					occur[number] += 1
 
 		for i in range(len(unique)):
 
 			if occur[unique[i]] >= 3: 
 				#equalilateral
 				if occur[unique[i]] == 3:
+					# solutions.append([unique[i],unique[i],unique[i]])
 					solCount += 1
 				else:
+					# solutions.append([unique[i],unique[i],unique[i]])
 					solCount += (self.factorial(occur[unique[i]]))/(6*self.factorial(occur[unique[i]]-3))
 
 			for j in range(i+1,len(unique)):
@@ -38,16 +45,26 @@ class Solution(object):
 					#iso
 					if 2*unique[i] > unique[j]:
 						if occur[unique[i]] == 2:
+							# solutions.append([unique[i],unique[i],unique[j]])
 							solCount += occur[unique[j]]
 						else:
+							# solutions.append([unique[i],unique[i],unique[j]])
 							solCount += ((self.factorial(occur[unique[i]]))/(2*self.factorial(occur[unique[i]]-2))*occur[unique[j]])
+
+				if occur[unique[j]] == 2:
+					# solutions.append([unique[i],unique[j],unique[j]])
+					solCount +=occur[unique[i]]
+				elif occur[unique[j]] > 2:
+					# solutions.append(['test',unique[i],unique[j],unique[j]])
+					solCount += ((self.factorial(occur[unique[j]]))/(2*self.factorial(occur[unique[j]]-2))*occur[unique[i]])
+
+				for k in range(j+1,len(unique)):
+					if (unique[i]+unique[j] > unique[k]) and (unique[j]+unique[k] > unique[i]) and (unique[k]+unique[i] > unique[j]):
+						# solutions.append([unique[i],unique[j],unique[k]])
+						solCount += occur[unique[i]]*occur[unique[j]]*occur[unique[k]]
 					else:
 						break
-
-				for k in range(i+2,len(unique)):
-					if (unique[i]+unique[j] > unique[k]) and (unique[j]+unique[k] > unique[i]) and (unique[k]+unique[i] > unique[j]):
-						solCount += occur[unique[i]]*occur[unique[j]]*occur[unique[k]]
-						break
+		# print(solutions)
 		return(int(solCount))
 
 	def factorial(self, a):
@@ -71,12 +88,16 @@ t3 = [82,10,63,49,72]
 t4 = [5,22,62,39,64]
 t5 = [1,40,98,77,85]
 t6 = [2,2,5,5,5,5,5]
+t7 = [2,2,5,5,5,5,5,0,0]
+t8 = [0,0,0]
 
 
 print('\n')
-print('\n',driver.triangleNumber(t1),"Answer = 3")
-print('\n',driver.triangleNumber(t2),"Answer = 51")
-print('\n',driver.triangleNumber(t3),"Answer = 5")
-print('\n',driver.triangleNumber(t4),"Answer = 3")
-print('\n',driver.triangleNumber(t5),"Answer = 4")
-print('\n',driver.triangleNumber(t6),"Answer = 10")
+print('t1',driver.triangleNumber(t1),"Answer = 3\n")
+print('t2',driver.triangleNumber(t2),"Answer = 51\n")
+print('t3',driver.triangleNumber(t3),"Answer = 5\n")
+print('t4',driver.triangleNumber(t4),"Answer = 3\n")
+print('t5',driver.triangleNumber(t5),"Answer = 4\n")
+print('t6',driver.triangleNumber(t6),"Answer = 30\n")
+print('t7',driver.triangleNumber(t7),"Answer = 30\n")
+print('t8',driver.triangleNumber(t8),"Answer = 0\n")
