3266ac4242f6683ba422a678c17a1ccdfe4e9dc3
mze5111@gmail.com <mze5111@gmail.com>
2017-08-22 20:18:46 -0400

    Accepted solution - top 94.8% runtime

---
+++ 67-Add-Binary.py
@@ -0,0 +1,11 @@
+# Problem: Given two binay strings
+# Return: The sum also in binary
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return("{0:b}".format(int(a,2)+int(b,2)))
