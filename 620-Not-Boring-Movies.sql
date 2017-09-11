221ed038d9cf8ce85a84d1f2d242e9f5c0dc18a5
Matthew <mze5111@gmail.com>
2017-09-11 17:43:57 -0400

    Added cinema table

--- 620-Not-Boring-Movies.sql
+++ 620-Not-Boring-Movies.sql
@@ -2,6 +2,14 @@
 
 -- Return: Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating. 
 
+Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1));
+Truncate table cinema;
+insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9');
+insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5');
+insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2');
+insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6');
+insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1');
+
 # Write your MySQL query statement below
 SELECT id,movie,description,rating
 FROM cinema
