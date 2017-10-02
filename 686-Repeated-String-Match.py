bc4f4da88f250a331d2f20f30b36e2137c4fa0e2
Matthew <mze5111@gmail.com>
2017-10-02 10:34:26 -0400

    Accepted solution

---
+++ 686-Repeated-String-Match.py
@@ -0,0 +1,24 @@
+'''
+Problem:
+
+Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. If no such solution, return -1.
+
+For example, with A = "abcd" and B = "cdabcdab".
+
+Return 3, because by repeating A three times (“abcdabcdabcd”), B is a substring of it; and B is not a substring of A repeated two times ("abcdabcd")
+'''
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: int
+        """
+        sol = 1
+        long_A = A
+        while B not in long_A:
+            if len(long_A) > len(B)*2 and sol > 1:
+                return(-1)
+            long_A+=A
+            sol+=1
+        return(sol)
