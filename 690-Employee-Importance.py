be08834d3ed232f1c9fc2ad3c3be11f0e0b8f175
Matthew <mze5111@gmail.com>
2017-10-16 15:11:45 -0400

    Accepted solution

---
+++ 690-Employee-Importance.py
@@ -0,0 +1,40 @@
+'''
+Problem:
+You are given a data structure of employee information, which includes the employee's unique id, his importance value and his direct subordinates' id.
+
+For example, employee 1 is the leader of employee 2, and employee 2 is the leader of employee 3. They have importance value 15, 10 and 5, respectively. Then employee 1 has a data structure like [1, 15, [2]], and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. Note that although employee 3 is also a subordinate of employee 1, the relationship is not direct.
+
+Now given the employee information of a company, and an employee id, you need to return the total importance value of this employee and all his subordinates.
+'''
+
+# Employee info
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        self.lookup = {}
+        for emp in employees:
+            self.lookup[emp.id] = emp
+            
+        interest = self.lookup[id]
+        return(self.importanceHelper(interest))
+        
+    def importanceHelper(self,employee):
+        value = employee.importance
+        for emp in employee.subordinates:
+            value += self.importanceHelper(self.lookup[emp])
+        return(value)
+        
