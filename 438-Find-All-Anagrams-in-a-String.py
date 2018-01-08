107dfc5bd4fb89b4097a0c43c7ec82af96476ad6
Matthew <mze5111@gmail.com>
2018-01-07 19:10:55 -0500

    Accepted solution

--- 438-Find-All-Anagrams-in-a-String.py
+++ 438-Find-All-Anagrams-in-a-String.py
@@ -1,45 +1,63 @@
 # Problem:
-# Given a singly linked list
+# Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
 
-# Return: 
-# All odd nodes together followed by the even nodes.
-# Odd nodes refer to placement in list, not value. The first node is odd, second even, ect.
+# Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
 
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# The order of output does not matter.
 
 class Solution(object):
-    def oddEvenList(self, head):
+    def findAnagrams(self, s, p):
         """
-        :type head: ListNode
-        :rtype: ListNode
+        :type s: str
+        :type p: str
+        :rtype: List[int]
         """
-        if not head:
-            return([])
+        sol = []
+
+        if len(s) < len(p):
+            return(sol)
+
+        key = self.char_count_arr(p)
         
-        count = 2
-        sol = ListNode(head.val)
-        solStart = sol
-        head = head.next
-        first = True
-        
-        while head:
-            if count % 2 == 0:
-                if first:
-                    even = ListNode(head.val)
-                    evenStart = even
-                    first = False
-                else:
-                    even.next = ListNode(head.val)
-                    even = even.next
-            else:
-                sol.next = ListNode(head.val)
-                sol = sol.next
-            head = head.next
-            count +=1
-        if not first:
-            sol.next = evenStart
-        return(solStart)
+        win_start = 0
+        win_end = win_start+len(p)
+        window = self.char_count_arr(s[win_start:win_end])
+
+        while win_end < len(s):
+            if window == key:
+                sol.append(win_start)
+            window[ord(s[win_start])-97] -= 1
+            window[ord(s[win_end])-97] += 1
+            win_start += 1
+            win_end +=1
+
+        if window == key:
+            sol.append(win_start)
+
+        return(sol)
+
+    def char_count_arr(self, string):
+        char_count = 26*[0]
+        for char in string:
+            char_count[ord(char)-97] += 1
+        return(char_count)
+
+
+driver = Solution()
+print('\n')
+
+print('1')
+print('**',driver.findAnagrams("cbaebabacd","abc"))
+print('-- [0, 6]\n')
+
+print('2')
+print('**',driver.findAnagrams("abab","ab"))
+print('-- [0, 1, 2]\n')
+
+print('3')
+print('**',driver.findAnagrams("a","ab"))
+print('-- []\n')
+
+print('4')
+print('**',driver.findAnagrams("aaaaaaa","a"))
+print('-- [0, 1, 2, 3, 4, 5, 6]\n')
